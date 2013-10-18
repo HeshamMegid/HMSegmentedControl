604a9dda2ddb7341683b2f457a73ecbb65d734e7
Hesham Abd-Elmegid <hesham.abdelmegid@gmail.com>
2013-10-18 15:17:48 +0200

    Removes nib file for demo view controller

--- HMSegmentedControlExample/HMSegmentedControlExample/AppDelegate.m
+++ HMSegmentedControlExample/HMSegmentedControlExample/AppDelegate.m
@@ -15,7 +15,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.viewController = [[ViewController alloc] init];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
