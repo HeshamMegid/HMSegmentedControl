2cf4b16a094c35685dbbe8644c4e9e8553199c90
Hesham Abd-Elmegid <hesham.abdelmegid@gmail.com>
2014-03-06 09:01:36 +0200

    Fixes selection indicator when using HMSegmentedControlSegmentWidthStyleDynamic with HMSegmentedControlSelectionStyleBox

--- HMSegmentedControl/HMSegmentedControl.m
+++ HMSegmentedControl/HMSegmentedControl.m
@@ -405,9 +405,11 @@
         
         NSInteger i = 0;
         for (NSNumber *width in self.segmentWidthsArray) {
-            selectedSegmentOffset = selectedSegmentOffset + [width floatValue];
-            if (self.selectedSegmentIndex == i)
+            if (self.selectedSegmentIndex == i) {
                 break;
+            }
+            selectedSegmentOffset = selectedSegmentOffset + [width floatValue];
+
             i++;
         }
         
