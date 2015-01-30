b87097d863c83b8fdcb6ec665c29c8219a6ddd43
Hesham Abd-Elmegid <hesham.abdelmegid@gmail.com>
2015-01-30 18:28:21 +0200

    Fix compiler warning due to uninitialized variable, via @fiam in #68.

--- HMSegmentedControl/HMSegmentedControl.m
+++ HMSegmentedControl/HMSegmentedControl.m
@@ -660,7 +660,7 @@
             }
         }
         
-        NSUInteger sectionsCount;
+        NSUInteger sectionsCount = 0;
         
         if (self.type == HMSegmentedControlTypeImages) {
             sectionsCount = [self.sectionImages count];
