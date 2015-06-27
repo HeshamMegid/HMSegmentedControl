4d25b63dfc7b259c4b24c99c3c8c033c4a492aed
Hesham Abd-Elmegid <hesham.abdelmegid@gmail.com>
2015-06-27 13:19:39 +0200

    Fixes a crash that happens on iOS 9 when loading STHeitiSC-Light font.

--- HMSegmentedControl/HMSegmentedControl.m
+++ HMSegmentedControl/HMSegmentedControl.m
@@ -859,7 +859,7 @@
 
 - (NSDictionary *)resultingTitleTextAttributes {
     NSDictionary *defaults = @{
-        NSFontAttributeName : [UIFont fontWithName:@"STHeitiSC-Light" size:18.0f],
+        NSFontAttributeName : [UIFont systemFontOfSize:19.0f],
         NSForegroundColorAttributeName : [UIColor blackColor],
     };
     
