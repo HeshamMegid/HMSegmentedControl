1f5ccd268e40e616caed96738133f22851c2d917
chrene <chrene91@gmail.com>
2015-09-10 22:41:09 +0200

    Add Carthage support
    - Refactored `HMSegmentedControl.h` and `HMSegmentedControl.m` into it's own framework
    - HMSegmentedControl Framework target = 8.0
    - HMSegmentedControl scheme is shared (needed for carthage)

--- HMSegmentedControl/HMSegmentedControl.h
+++ HMSegmentedControl/HMSegmentedControl.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+//! Project version number for HMSegmentedControl.
+FOUNDATION_EXPORT double HMSegmentedControlVersionNumber;
+
+//! Project version string for HMSegmentedControl.
+FOUNDATION_EXPORT const unsigned char HMSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMSegmentedControl/PublicHeader.h>
+
+
+
 @class HMSegmentedControl;
 
 typedef void (^IndexChangeBlock)(NSInteger index);
--- HMSegmentedControlExample/HMSegmentedControlExample/ViewController.h
+++ HMSegmentedControlExample/HMSegmentedControlExample/ViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "HMSegmentedControl.h"
+#import <HMSegmentedControl/HMSegmentedControl.h>
 
 @interface ViewController : UIViewController <UIScrollViewDelegate>
 
